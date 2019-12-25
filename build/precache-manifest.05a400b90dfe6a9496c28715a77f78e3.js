1c3f086989fced9517bafad34f8e4e87ead48694
sarapourabbas <sara.poorabas@gmail.com>
2019-12-25 12:28:25 +0330

    add github pages

---
+++ build/precache-manifest.05a400b90dfe6a9496c28715a77f78e3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f89cad0bdcdfa6da432b3a40bf4b21f",
+    "url": "/test-job/index.html"
+  },
+  {
+    "revision": "131ce6ffd13eea20f645",
+    "url": "/test-job/static/css/2.c0dd88e9.chunk.css"
+  },
+  {
+    "revision": "9cc787b1dda4926cd77c",
+    "url": "/test-job/static/css/main.0caca8c2.chunk.css"
+  },
+  {
+    "revision": "131ce6ffd13eea20f645",
+    "url": "/test-job/static/js/2.6d441dcf.chunk.js"
+  },
+  {
+    "revision": "9cc787b1dda4926cd77c",
+    "url": "/test-job/static/js/main.aa3052ee.chunk.js"
+  },
+  {
+    "revision": "7988674609592e188f66",
+    "url": "/test-job/static/js/runtime-main.4ac22916.js"
+  },
+  {
+    "revision": "8be1620010705154fe07eff863584552",
+    "url": "/test-job/static/media/Logo2.8be16200.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/test-job/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/test-job/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/test-job/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/test-job/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/test-job/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "13399d0a15a564723a772926964cb4d1",
+    "url": "/test-job/static/media/gift-card.13399d0a.jpg"
+  },
+  {
+    "revision": "48cd7576b1b353728309d3e91afc16ea",
+    "url": "/test-job/static/media/logo.48cd7576.png"
+  },
+  {
+    "revision": "d4396cb23ddbdaca1ae29a1d04748a32",
+    "url": "/test-job/static/media/slider-back.d4396cb2.jpg"
+  },
+  {
+    "revision": "15ae772499285cd1361e0d4035138124",
+    "url": "/test-job/static/media/trebuc.15ae7724.woff"
+  }
+]);
