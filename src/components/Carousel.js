8eef8ff17d0e19706f569a4d4eddfdf7053f6759
sarapourabbas <sara.poorabas@gmail.com>
2021-09-03 14:07:14 +0430

    update project

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,133 +1,94 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 export class Carousel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentImageIndex: 0,
-            items: []
-        };
-    }
-    componentDidMount(){
-        this.setState({items:this.props.items})
-    }
-
-    prevSlide = () => {
-        const lastIndex = this.state.items.length - 1;
-
-        const resetIndex = this.state.currentImageIndex === 0;
-        const index = resetIndex ? lastIndex : this.state.currentImageIndex - 1;
-
-        this.setState({
-            currentImageIndex: index
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImageIndex: 0,
+      items: [],
     };
-    nextSlide = () => {
-        const lastIndex = this.state.items.length - 1;
+  }
+  componentDidMount() {
+    this.setState({ items: this.props.items });
+  }
 
-        const resetIndex = this.state.currentImageIndex === lastIndex;
-        const index = resetIndex ? 0 : this.state.currentImageIndex + 1;
+  prevSlide = () => {
+    const lastIndex = this.state.items.length - 1;
 
-        this.setState({
-            currentImageIndex: index
-        });
-    };
+    const resetIndex = this.state.currentImageIndex === 0;
+    const index = resetIndex ? lastIndex : this.state.currentImageIndex - 1;
 
+    this.setState({
+      currentImageIndex: index,
+    });
+  };
+  nextSlide = () => {
+    const lastIndex = this.state.items.length - 1;
 
-    render(){
-        const index = this.state.currentImageIndex;
+    const resetIndex = this.state.currentImageIndex === lastIndex;
+    const index = resetIndex ? 0 : this.state.currentImageIndex + 1;
 
-        let firstFiveVideo = this.state.items.slice(index, index + 3);
+    this.setState({
+      currentImageIndex: index,
+    });
+  };
 
-        if (firstFiveVideo.length < 3) {
-            firstFiveVideo = firstFiveVideo.concat(
-                this.state.items.slice(0,3 - firstFiveVideo.length)
-            );
-        }
-        return(
-            <div className='container-fluid'>
-                <div className='position-relative'>
-                    <div className='carousel-inner row flex-nowrap align-items-center'>
+  render() {
+    const index = this.state.currentImageIndex;
 
-                        <div className='arrow-carousel carousel-left' onClick={this.prevSlide}  >
-                            <i className='fa fa-chevron-left'/>
-                        </div>
-                        {firstFiveVideo.map((el, index) => (
-                        <div className='carousel-item-custom ' key={index}>
-                            <a className='d-inline-block'>
-                                <img  src={el.image} alt="" />
-                            </a>
-                            <div className='carousel-info'>
-                                <div className='title'>{el.title}</div>
-                                <div className='sub-title'>
-                                    <a className='main'>{el.menuType.title}</a>
-                                    <a className='sub'>{el.cuisineType.title}</a>
-                                    <a className='sub'>{el.courseType.title}</a>
-                                </div>
-                                <div className='time-info'>
-                                <span className='text-left'>
-                                  <span className='tag light'>
-                                    <i className='fa fa-clock-o'/>
-                                  Mins
-                                  </span>
-                                  <span className='tag dark'>${el.price}</span>
-                                </span>
-                                    <span className='tag light free green-tag'>{el.isAcceptingDelivery?"freeDelivery":"freePickup"}</span>
-                                </div>
-                            </div>
-                        </div>
-                        ))}
+    let firstFiveVideo = this.state.items.slice(index, index + 3);
 
-                        <div className='arrow-carousel carousel-right'  onClick={this.nextSlide}>
-                            <i className='fa fa-chevron-right'/>
-                        </div>
-
-                        </div>
-
-                    </div>
+    if (firstFiveVideo.length < 3) {
+      firstFiveVideo = firstFiveVideo.concat(
+        this.state.items.slice(0, 3 - firstFiveVideo.length)
+      );
+    }
+    return (
+      <div className="container-fluid">
+        <div className="position-relative">
+          <div className="carousel-inner row flex-nowrap align-items-center">
+            <div
+              className="arrow-carousel carousel-left"
+              onClick={this.prevSlide}
+            >
+              <i className="fa fa-chevron-left" />
+            </div>
+            {firstFiveVideo.map((el, index) => (
+              <div className="carousel-item-custom " key={index}>
+                <a className="d-inline-block">
+                  <img src={el.image} alt="" />
+                </a>
+                <div className="carousel-info">
+                  <div className="title">{el.title}</div>
+                  <div className="sub-title">
+                    <a className="main">{el.menuType.title}</a>
+                    <a className="sub">{el.cuisineType.title}</a>
+                    <a className="sub">{el.courseType.title}</a>
+                  </div>
+                  <div className="time-info">
+                    <span className="text-left">
+                      <span className="tag light">
+                        <i className="fa fa-clock-o" />
+                        Mins
+                      </span>
+                      <span className="tag dark">${el.price}</span>
+                    </span>
+                    <span className="tag light free green-tag">
+                      {el.isAcceptingDelivery ? "freeDelivery" : "freePickup"}
+                    </span>
+                  </div>
                 </div>
+              </div>
+            ))}
 
-
-
-
-
-           /* {/!*<div className='carousel-box'>
-                    <div className='arrow-carousel carousel-left' onClick={this.prevSlide}  >
-                        <i className='fa fa-chevron-left'/>
-                    </div>
-                        {firstFiveVideo.map((el, index) => (
-                            <div className='carousel-list px-1' key={index}>
-                                <div className='image-list'>
-                                    <a className='d-inline-block'>
-                                        <img  src={el.image} alt="" />
-                                    </a>
-                                </div>
-                                <div className='carousel-info'>
-                                    <div className='title'>{el.title}</div>
-                                    <div className='sub-title'>
-                                        <a className='main'>{el.menuType.title}</a>
-                                        <a className='sub'>{el.cuisineType.title}</a>
-                                        <a className='sub'>{el.courseType.title}</a>
-                                    </div>
-                                    <div className='time-info'>
-                                <span className='text-left'>
-                                  <span className='tag light'>
-                                    <i className='fa fa-clock-o'/>
-                                  Mins
-                                  </span>
-                                  <span className='tag dark'>${el.price}</span>
-                                </span>
-                                        <span className='tag light free green-tag'>{el.isAcceptingDelivery?this.props.freeDelivery:this.props.freePickup}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-
-                    <div className='arrow-carousel carousel-right'  onClick={this.nextSlide}>
-                        <i className='fa fa-chevron-right'/>
-                    </div>
-            </div>*!/}*/
-        )
-    }
-     }
-
-
+            <div
+              className="arrow-carousel carousel-right"
+              onClick={this.nextSlide}
+            >
+              <i className="fa fa-chevron-right" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,125 +1,140 @@
-import React, {Component} from "react";
-import {Button} from '@material-ui/core';
+import React, { Component } from "react";
+import { Button } from "@material-ui/core";
 import { Carousel } from "./Carousel";
 import myJson from "../menu.json";
-import giftCard from '../assets/images/gift-card.jpg';
-import logo from '../assets/images/Logo2.png';
-import Container from '@material-ui/core/Container';
+import giftCard from "../assets/images/gift-card.jpg";
+import logo from "../assets/images/Logo2.png";
+import Container from "@material-ui/core/Container";
 
+class CarouselItem extends Component {
+  state = {
+    typeMarker: "delivery",
+    free: [],
+  };
+  deliveryMarker = () => {
+    let freeDelivery = myJson.filter((a) => a.isAcceptingDelivery === true);
+    this.setState({
+      typeMarker: "delivery",
+      // ,free:freeDelivery
+    });
+  };
 
-class CarouselItem extends Component{
-    state={
-        typeMarker:'delivery',
-        free:[],
-    };
-    deliveryMarker=()=>{
-        let freeDelivery= myJson.filter(a=> a.isAcceptingDelivery===true);
-        this.setState({typeMarker:'delivery'
-           // ,free:freeDelivery
-        });
-    };
+  pickupMarker = () => {
+    let freePickup = myJson.filter((a) => a.isAcceptingDelivery === false);
+    this.setState({
+      typeMarker: "pickup",
+      //,free:freePickup
+    });
+  };
 
-    pickupMarker=()=>{
-        let freePickup= myJson.filter(a=>a.isAcceptingDelivery===false);
-        this.setState({typeMarker:'pickup'
-            //,free:freePickup
-        })
-    };
-
-    componentDidMount(){
-        let freeDelivery= myJson.filter(a=> a.isAcceptingDelivery===true);
-        this.setState({typeMarker:'delivery'
-           // ,free:freeDelivery
-        },()=>
-        {
-            console.log('free',this.state.free)
-        });
-
-    }
-    render() {
-        return (
-            <Container maxWidth="lg" className='middle-contain px-0'>
-                <div className='middle-contain-head'>
-                    <div className='middle-contain-head-menu'>
-                        <a className='order-options'>
-                            <div className='mode'>ASAP Delivery</div>
-                            <div className='no-address mt-1'>What's Your Address ?</div>
-                        </a>
-                        <div className=' d-inline-block '>
-                            <Button className='change-button'> Change </Button>
-                        </div>
-                        <div className='type-select'>
-                            <div className={`marker ${this.state.typeMarker==='delivery' ? 'marker-delivery':'marker-pickup'} `} />
-                            <div className='type-item' onClick={this.deliveryMarker}>Delivery</div>
-                            <span className='sep mx-2'>Or</span>
-                            <div className='type-item'onClick={this.pickupMarker}>Pickup</div>
-                        </div>
-                        <div className='divider' />
-                        <a className='catering'>Catering</a>
-                    </div>
+  componentDidMount() {
+    let freeDelivery = myJson.filter((a) => a.isAcceptingDelivery === true);
+    this.setState(
+      {
+        typeMarker: "delivery",
+        // ,free:freeDelivery
+      },
+      () => {
+        console.log("free", this.state.free);
+      }
+    );
+  }
+  render() {
+    return (
+      <Container maxWidth="lg" className="middle-contain px-0">
+        <div className="middle-contain-head">
+          <div className="middle-contain-head-menu">
+            <a className="order-options">
+              <div className="mode">ASAP Delivery</div>
+              <div className="no-address mt-1">What's Your Address ?</div>
+            </a>
+            <div className=" d-inline-block ">
+              <Button className="change-button"> Change </Button>
+            </div>
+            <div className="type-select">
+              <div
+                className={`marker ${
+                  this.state.typeMarker === "delivery"
+                    ? "marker-delivery"
+                    : "marker-pickup"
+                } `}
+              />
+              <div className="type-item" onClick={this.deliveryMarker}>
+                Delivery
+              </div>
+              <span className="sep mx-2">Or</span>
+              <div className="type-item" onClick={this.pickupMarker}>
+                Pickup
+              </div>
+            </div>
+            <div className="divider" />
+            <a className="catering">Catering</a>
+          </div>
+        </div>
+        <div className="carousel-content">
+          <div className="items-contain">
+            <div className=" mt-4">
+              <div className="title-carousel"> Salad </div>
+              <div className="scrolling-carousel" />
+              <Carousel items={myJson} />
+            </div>
+            <div className="gift-card py-4">
+              <div className="gift-image p-4">
+                <img src={giftCard} alt="gift-card" />
+              </div>
+              <div className="gift-body">
+                <div>
+                  <span className="branded">Branded</span>
+                  <span className="giftCard ml-2">Gift Card</span>
                 </div>
-                <div className='carousel-content'>
-
-                    <div className='items-contain'>
-                        <div className=' mt-4'>
-                            <div className='title-carousel'> Salad </div>
-                            <div className='scrolling-carousel' />
-                            <Carousel  items={myJson} />
-
-                        </div>
-                        <div className='gift-card py-4'>
-                            <div className='gift-image p-4'>
-                                <img src={giftCard} alt='gift-card'/>
-                            </div>
-                            <div className='gift-body'>
-                                <div>
-                                    <span className='branded'>Branded</span>
-                                    <span className='giftCard ml-2'>Gift Card</span>
-                                </div>
-                                <div className='logo-bottom mb-2'>
-                                    <img src={logo} alt='logo'/>
-                                </div>
-                                <div className='desc text-justify'>
-                                    <p className='mb-0'>
-                                        Opeqe provides a wide range of customizable products for reward and incentive programs that can meet your restaurant’s goals.
-                                    </p>
-                                    <p>
-                                        Whether you are looking to drive the addition of new customers, increase the loyalty of existing ones we have a customized solution for you.
-                                    </p>
-                                </div>
-                                <div className='quote text-left'>
-                                    <Button className='quote-btn'> GET ONLINE QUOTE </Button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='ready py-4'>
-                            <div className='title-ready'> Ready to Order </div>
-                            <div className='title-ready-sub mt-2'>Browse our menu for dine-in, delivery or pickup and catering</div>
-                            <div className='bottom-menu'>
-                                <a className='order-options'>
-                                    <div className='mode'>ASAP Delivery</div>
-                                    <div className='no-address mt-1'>What's Your Address ?</div>
-                                </a>
-                                <div className=' d-inline-block '>
-                                    <Button className='change-button'> Change </Button>
-                                </div>
-                                <div className='type-select'>
-                                    <div className='marker' />
-                                    <div className='type-item'>Delivery</div>
-                                    <span className='sep mx-2'>Or</span>
-                                    <div className='type-item'>Pickup</div>
-                                </div>
-                                <div className='divider' />
-                                <a className='catering'>Catering</a>
-                            </div>
-                        </div>
-
-                    </div>
+                <div className="logo-bottom mb-2">
+                  <img src={logo} alt="logo" />
                 </div>
-
-            </Container>
-        );
-    }
+                <div className="desc text-justify">
+                  <p className="mb-0">
+                    Opeqe provides a wide range of customizable products for
+                    reward and incentive programs that can meet your
+                    restaurant’s goals.
+                  </p>
+                  <p>
+                    Whether you are looking to drive the addition of new
+                    customers, increase the loyalty of existing ones we have a
+                    customized solution for you.
+                  </p>
+                </div>
+                <div className="quote text-left">
+                  <Button className="quote-btn"> GET ONLINE QUOTE </Button>
+                </div>
+              </div>
+            </div>
+            <div className="ready py-4">
+              <div className="title-ready"> Ready to Order </div>
+              <div className="title-ready-sub mt-2">
+                Browse our menu for dine-in, delivery or pickup and catering
+              </div>
+              <div className="bottom-menu">
+                <a className="order-options">
+                  <div className="mode">ASAP Delivery</div>
+                  <div className="no-address mt-1">What's Your Address ?</div>
+                </a>
+                <div className=" d-inline-block ">
+                  <Button className="change-button"> Change </Button>
+                </div>
+                <div className="type-select">
+                  <div className="marker" />
+                  <div className="type-item">Delivery</div>
+                  <span className="sep mx-2">Or</span>
+                  <div className="type-item">Pickup</div>
+                </div>
+                <div className="divider" />
+                <a className="catering">Catering</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default CarouselItem;
